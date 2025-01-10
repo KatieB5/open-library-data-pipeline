7970bf950c6191d18c3bded9b31a74d1e541806b
Katie Bickford <katie.bickford@thedatalab.org>
2025-01-10 11:14:27 +0000

    Add test for extract function with mock API called
    The OpenLibraryAPI is rate limited. The datapilines.py project calls the API multiple times. In order to test locally without spamming the OpenLibraryAPI, mocking has been implemented.
    
    To facilitate mocking, pytest's monkeypatching feature has been utilised.
    
    This is a first attempt using this functionality. Future refactoring is anticipated.

---
+++ test_datapipelines.py
@@ -0,0 +1,26 @@
+import pytest
+import httpx
+import json
+import itertools
+
+from data_pipelines import extract
+
+
+class MockLibraryApiResponse:
+    @staticmethod
+    def json():
+        with open("open_library_api_response.json") as f:
+            data = json.load(f)
+
+        return data
+
+
+def test_extract(monkeypatch):
+    def mock_get_json(*args, **kwargs):
+        return MockLibraryApiResponse()
+
+    monkeypatch.setattr(httpx, "get", mock_get_json)
+
+    records = extract()
+    first_record = next(iter(records))
+    assert first_record["title"] == "The Lord of the Rings"
